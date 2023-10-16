eb9c0f3c5b02fc0c0b6fc84a7233575e6c29aecd
MarinaMiron <marina.miron00@e-uvt.ro>
2023-10-16 09:28:37 +0300

    Day 7 - Hangman project

--- Day7/hangman_final.py
+++ Day7/hangman_final.py
@@ -41,7 +41,7 @@ while not end_of_game:
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("You lost. :(")
+            print("You died. :(")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
