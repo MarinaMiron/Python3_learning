3482c685f31b5cddc932c0273a7aa0b24bdc5047
MarinaMiron <marina.miron00@e-uvt.ro>
2023-10-30 10:13:04 +0200

    Day 16

--- Day16/day_16_start.py
+++ Day16/day_16_start.py
@@ -6,14 +6,15 @@
 # timmy.shapesize(1.5)
 # timmy.color("DarkSlateGray")
 # timmy.forward(100)
+# timmy.circle(50)
 
 # my_screen = Screen()
-# print(my_screen.canvheight)
+# #print(my_screen.canvheight)
 # my_screen.exitonclick()
 
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electic", "Water", "Fire"])
-table.align = "l"
-print(table)
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electic", "Water", "Fire"])
+# table.align = "l"
+# print(table)
