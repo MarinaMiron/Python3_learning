b53299752ad4b836f910556ca8b60b75545de9c4
MarinaMiron <marina.miron00@e-uvt.ro>
2023-11-28 17:12:52 +0200

    Day 19- Turtle Race game

--- Day19/turtle_race.py
+++ Day19/turtle_race.py
@@ -6,17 +6,18 @@ screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color:")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-turtle_y= -70
+turtle_y= -100
 all_turtles = []
 
 #Create 6 turtles
 for turtle_index in range(0, 6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.color(colors[turtle_index])
+    new_turtle.shapesize(2)
     new_turtle.penup()
     new_turtle.goto(x=-230, y=turtle_y)
     all_turtles.append(new_turtle)
-    turtle_y+= 30
+    turtle_y+= 40
 
 while user_bet:
     is_race_on = True
