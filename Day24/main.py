66f22bb1f79c8311605f99ea4941fb80dd94efa0
MarinaMiron <marina.miron00@e-uvt.ro>
2023-12-09 16:46:13 +0200

    Day 24 - File manipulation

---
+++ Day24/main.py
@@ -0,0 +1,10 @@
+# with open("Day24\my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("Day24\my_file.txt", mode="a") as file:
+#     file.write("\nNew text.")
+
+with open("new_file.txt", mode="w") as file:
+    file.write("New text.")
+
